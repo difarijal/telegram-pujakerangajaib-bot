e30db0c8ad22b84550edaedfcf5d5b4170634d97
Ridho Fauzan <ridhofauzan92@gmail.com>
2017-11-20 14:27:27 +0700

    added 1 new validation for asking questions

--- controllers/otherwise.js
+++ controllers/otherwise.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const   Telegram = require('telegram-node-bot');
+const Telegram = require('telegram-node-bot');
 
 class OtherwiseController extends Telegram.TelegramBaseController {
-    handler($) {
-        $.sendMessage('Maap bray ga ngerti');
+    handle($) {
+        var textArray = [
+            'Yoi Bray',
+            'Kaga Bray'
+        ];
+        var randomNumber = Math.floor(Math.random()*textArray.length);
+        
+        let message = $.message.text.split(' ');
+        let validate_word_1 = message[0].toLowerCase();
+
+        if(validate_word_1 === 'apakah'){
+            $.sendMessage(textArray[randomNumber]);
+        }else{
+            //$.sendMessage('Lau ngomong apa sob?');
+        }
     }
 }
-
 module.exports = OtherwiseController;
